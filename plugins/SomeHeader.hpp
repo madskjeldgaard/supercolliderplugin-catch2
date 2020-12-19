7de83c64838effde6a214c9c79e30e9b4a0c9a2c
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2020-12-19 01:17:17 +0100

    someheader

---
+++ plugins/SomeHeader.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "SC_PlugIn.hpp"
+
+namespace SomeHeader {
+
+class SomeHeader {
+public:
+  int twentyTwo() { return 22; }
+private:
+};
+
+} // namespace SomeHeader 
--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,15 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this
                           // once
 #include "SC_Constants.h"
+#include "SomeHeader.hpp"
 #include "catch2/catch.hpp"
-//
+
 // A silly test to see if the plugin's pi is equivalent to M_PI
 TEST_CASE("See if Pi is okay", "[pi check]") { REQUIRE(pi == M_PI); }
+
+// Another silly test demonstrating plugins/SomeHeader.hpp
+TEST_CASE("See if the twentyTwo method returns 22", "[twentytwo]") {
+  SomeHeader::SomeHeader sh;
+  int num = sh.twentyTwo();
+  REQUIRE(num == 22);
+}
